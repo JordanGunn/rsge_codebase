f5095e35ce5f14c304d17e47dcbeba60b858cf91
Jordan Godau <jordangunn92@gmail.com>
2023-10-11 14:50:32 -0700

    Restructured django project initialization and aux. files

--- lidar_program/Eclipse/server/eclipseserver/eclipseserver/asgi.py
+++ lidar_program/Eclipse/server/eclipseserver/eclipseserver/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for eclipse project.
+ASGI config for eclipseserver project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eclipse.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eclipseserver.settings')
 
 application = get_asgi_application()
--- lidar_program/Eclipse/server/eclipseserver/eclipseserver/wsgi.py
+++ lidar_program/Eclipse/server/eclipseserver/eclipseserver/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for eclipse project.
+WSGI config for eclipseserver project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eclipse.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eclipseserver.settings')
 
 application = get_wsgi_application()
