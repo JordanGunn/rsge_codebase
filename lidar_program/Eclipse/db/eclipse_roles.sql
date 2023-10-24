ec093ac50ed356bbe9f712ba37e20a5340e4667d
Jordan Godau <jordangunn92@gmail.com>
2023-10-24 09:46:56 -0700

    Deleted some useless files

--- lidar_program/Eclipse/db/eclipse_roles.sql
+++
@@ -1,8 +0,0 @@
--- Create new user role
-CREATE ROLE jordan WITH LOGIN PASSWORD 'password';
-
--- Grant all privileges on 'eclipse' database to the new role
-GRANT ALL PRIVILEGES ON DATABASE eclipse TO jordan;
-
--- (Optional) Set the role to be the owner of the 'eclipse' database
-ALTER DATABASE eclipse OWNER TO jordan;
